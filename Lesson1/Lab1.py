a644d7d1d2b86ad317a3f5ea85864c091d136924
foxb6 <foxb6@go.stockton.edu>
2023-09-12 19:21:33 -0400

    Completed Lab 1

--- Lesson1/Lab1.py
+++ Lesson1/Lab1.py
@@ -6,9 +6,13 @@
 # Note: to "uncomment a line", remove the # at the beginning of the line. The shortcut is to select the line and press Ctrl + / (windows) or Command + / (mac)
 # Note2: to run the program, Select the "Play" button at the top of the screen.
 
-#print("Hello World!")
+print("Hello World!")
 
 # 2. Add a print statement that prints your name. Run the program and show the output to your instructor.
 
+print("Bodhi Fox")
+
 # 3. Add a print statement that prints your favorite color. Run the program and show the output to your instructor.
+
+print("blue")
 
