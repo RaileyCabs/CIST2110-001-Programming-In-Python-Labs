cb4587763aa7c6011cd4e0d8b184a1fefde2226a
foxb6 <foxb6@go.stockton.edu>
2023-11-21 15:22:09 -0500

    Finished Lab 9

--- Week11/Lab9.py
+++ Week11/Lab9.py
@@ -1,5 +1,5 @@
 #Lab 9
-# Author:
+# Author: Bodhi Fox
 
 """_summary_
 
@@ -10,18 +10,27 @@ This lab is designed to create a simple web application using streamlit and crea
 # pip install streamlit
 
 # 2. Import streamlit here as st and datetime as dt
+import streamlit as st
+import datetime as dt
 
 
 # 3. Create a title for your web application. Streamlit has a function for this called title
 
+st.title("Date Counter Web Application")
 
 # 4. Create a subheader for your web application. Streamlit has a function for this called subheader
+
+st.subheader("This web app will count the number of days until a certain date")
 
 # 5. Create a date input for the user to enter a date. Streamlit has a function for this called date_input
 # Make sure to save the input into a variable
 
+data= st.date_input("Enter a date: ")
+
 # 6. Create a button for the user to click. Streamlit has a function for this called button
 # Make sure to save the button click into a variable
+
+button= st.button("Calculate")
 
 # 7. Create a function that will calculate the number of days until the date entered by the user.
 # You will need to use the datetime library for this.
@@ -33,12 +42,34 @@ This lab is designed to create a simple web application using streamlit and crea
 # The function should return an integer.
 # The function should be called calculate_days
 
+def calculate_days(date)->int:
+    current_date= dt.datetime.now().date()
+    days_difference= date- current_date
+    if days_difference.days < 0:
+        raise ValueError("The date entered is in the past")
+    return days_difference.days
+
+
 
 
 # 8. Create an app function that will run the web application.
 # Check if the button has been clicked, then call the calculate_days function and pass in the date entered by the user. Use a try except block to catch any errors.
 # Save the result into a variable.
 # Print out the result.
+def app():
+    if button:
+        try:
+           result= calculate_days(date)
+        except ValueError:
+            st.write("Please enter a valid date.")
+            return
+
+        st.write(f"Current date: {dt.datetime.now().date()}")
+        st.write(f"Selected Date: {date}")
+        st.write(f"Days until selected date: {result}")
+
+
+
 
 
 # 9. Run the web application by typing streamlit run Lab9.py in the terminal.
